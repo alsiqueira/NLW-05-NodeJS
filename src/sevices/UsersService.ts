88efa251ea7663e88c1994b5fe15cce23336733a
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-04-21 10:11:44 -0300

    Create entities Users

---
+++ src/sevices/UsersService.ts
@@ -0,0 +1,25 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepository } from "../repositories/UsersRepository"
+
+interface IUsersCreate {
+  email: string;
+
+}
+
+class UsersService {
+  async create({email}: IUsersCreate){
+    const usersRepository = getCustomRepository(UsersRepository)
+
+    const userAlreadyExists = await usersRepository.findOne({email})
+    if(userAlreadyExists){
+      throw new Error("Email already exists!")
+    }
+
+    const users = usersRepository.create({email})
+
+    await usersRepository.save(users)
+    return users
+  }
+}
+
+export {UsersService}
